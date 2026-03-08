5791fd32dd4bc31c7986e6779d560ff7dee1e71a
Josh Bailey <josh@vandervecken.com>
2026-03-08 07:31:43 +0000

    sw.

--- python/iqtlabs/qa_retune_fft.py
+++ python/iqtlabs/qa_retune_fft.py
@@ -586,6 +586,7 @@ class qa_getters(gr_unittest.TestCase):
             low_power_hold_down=False,
             slew_rx_time=False,
             peak_fft_range=0,
+            antenna_switch="",
         )
         self.assertTrue(iqtlabs_retune_fft_0.get_stare_mode())
         self.assertEqual(1.5e6, iqtlabs_retune_fft_0.get_tune_freq())
